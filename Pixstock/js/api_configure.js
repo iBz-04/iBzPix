98672eb4bf02a3fc25921829d7fb9c0e89c0a076
iBz-04 <issakaibrahimrayamah@gmail.com>
2024-08-13 20:12:10 +0300

    configurations updated

--- Pixstock/js/api_configure.js
+++ Pixstock/js/api_configure.js
@@ -13,7 +13,7 @@ import { urlEncode } from "./utils/urlEncode.js";
 
 
 const /** {String} */ API_KEY =
-    "4yK8ykS0l342kAeAebLQiZs8PmUj5R2u99ZO7L7yTpZF0BfDTK1Rnmkl";
+    "API_KEY_GOES_HERE";
 
 const /** {Object} */ headers = new Headers();
 headers.append("Authorization", API_KEY);
